3587b6fb7d57241ceb10aa49bdbe42c74f0140ba
MarosPC <maros.pekarik@pm.me>
2022-05-24 13:40:38 +0200

    animation example

--- TsProj/main-ui/index.tsx
+++ TsProj/main-ui/index.tsx
@@ -6,7 +6,9 @@ import {$ref, $unref, $set, argv, on, toManualReleaseDelegate, releaseManualRele
 import * as React from 'react';
 import { TextBlock, EditableTextBox, VerticalBox, CanvasPanel, ReactUMG, CanvasPanelSlot, Button, HorizontalBox, TextureImage } from 'react-umg';
 import { VectorEditable } from './ui-components'
+import * as data from './004.json'
 
+let obj = new UE.MainObject();
 
 let SlotOfVerticalBox: CanvasPanelSlot = {
     LayoutData: {
@@ -19,6 +21,38 @@ let SlotOfVerticalBox: CanvasPanelSlot = {
     }
 }
 
+const sleep = (time) => {
+    return new Promise((resolve) => setTimeout(resolve, time))
+  }
+
+const animate = async (d, testStruct, bpActor, index) => {
+    let positions_array = UE.NewArray(UE.Vector);
+    let triangles_array = UE.NewArray(UE.BuiltinInt);
+    if (Object.entries(d).length > index) {
+        let entry = Object.entries(d)[index]
+        let key = entry[0]
+        let value = entry[1]
+
+        value['positions'].forEach((v, i) => {
+            positions_array.Add(new UE.Vector(v[0] * 100, v[1] * 100, v[2] * 100));
+        })
+        value['faces'].forEach((v, i) => {
+            v.forEach((g) => {
+                triangles_array.Add(g);
+            });
+        });
+  
+        sleep(40).then(() => {
+            testStruct.MyArray = positions_array;
+            testStruct.Triangles = triangles_array;
+            let incr = index + 1;
+            bpActor.Render(testStruct);
+
+            animate(d, testStruct, bpActor, incr);
+        });
+    }
+}
+
 
 function Main() {
     const [text, setText] = useState('First model');
@@ -28,10 +62,15 @@ function Main() {
     
     // From here you can access the blueprint class
     let bpActor = world.SpawnActor(bpClass, undefined, UE.ESpawnActorCollisionHandlingMethod.Undefined, undefined, undefined) as UE.TestBlueprint_C;
-    bpActor.Bar(1, 1, 1);
     bpActor.SetActorScale3D(new UE.Vector(7, 7, 7));	
     bpActor.K2_SetActorLocation(new UE.Vector(0,1,1), undefined, undefined, undefined);
+    let TestStruct = UE.UserDefinedStruct.Load("UserDefinedStruct'/Game/StarterContent/TestStruct.TestStruct'");
+    let testStruct = UE.NewStruct(TestStruct) as UE.TestStruct;
+ 
+    let json_object = JSON.parse(JSON.stringify(data));
 
+    animate(json_object, testStruct, bpActor, 0)
+    
     return (<CanvasPanel>
         <VerticalBox Slot={SlotOfVerticalBox}>
             <EditableTextBox Text={text} OnTextChanged={value=> setText(value)}/>
